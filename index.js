12bf6ec62cf126c19d4b45c9ea1a0138d27589f2
Yajie He <jesshyj@outlook.com>
2025-01-26 10:00:18 -0600

    modified

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function greet() {
+    var name = prompt("What is your name?");
+  
+    alert("Hello, " + name);
+  
+    var age = prompt("How old are you?");
+  
+    var ageNumber = parseInt(age);
+  
+    var hadBirthday = confirm("Have you had your birthday this year?");
+  
+    var currentYear = new Date().getFullYear();
+  
+    var birthYear;
+    if (hadBirthday) {
+      birthYear = currentYear - ageNumber;
+    } else {
+      birthYear = currentYear - ageNumber - 1;
+    }
+  
+    alert("You were born in " + birthYear + ".");
+  }
